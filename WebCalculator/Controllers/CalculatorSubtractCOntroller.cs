daabab94be31315929bb0d98e3ec8489c00b06a8
Francisco <a00223609@mycambrian.ca>
2022-07-12 19:01:37 -0230

    #SubtractController

---
+++ WebCalculator/Controllers/CalculatorSubtractCOntroller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalculatorLogic;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorSubtractController : ControllerBase
+    {
+        //Get numbers to subtract
+        [HttpGet]
+        public double Subtract([FromQuery] double num1, [FromQuery] double num2)
+        {
+            return Class1.Subtraction(num1, num2);
+        }
+
+    }
+}
