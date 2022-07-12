f6eb738935713c0b58daedc0d2454ca8971b68a5
Francisco <a00223609@mycambrian.ca>
2022-07-12 18:18:54 -0230

    Add project files.

---
+++ CalculatorLogic/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLogic
+{
+    public class Class1
+    {
+        //Addition
+        public static double Addition(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        //Substraction
+        public static double Subtraction(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        //Multiplication
+        public static double Multiplication(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        //Division
+        public static double Division(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return num1 / num2;
+            }
+        }
+    }
+}
+
