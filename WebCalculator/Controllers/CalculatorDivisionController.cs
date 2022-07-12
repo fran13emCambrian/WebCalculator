ff651fc46f1dcd73484a1b36d16847c37841b270
Francisco <a00223609@mycambrian.ca>
2022-07-12 18:45:40 -0230

    #Add and DIvide COntrollers

---
+++ WebCalculator/Controllers/CalculatorDivisionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalculatorLogic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+
+namespace WebCalculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorDivisionController : ControllerBase
+    {
+        //Takes numbers for the division
+       public double Division([FromQuery] double num1, [FromQuery] double num2)
+       {
+      return Class1.Division(num1, num2);
+        }
+    }
+}
